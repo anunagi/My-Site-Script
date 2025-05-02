bf10190e6ada904c64d970e2b16fe80935dcb1c2
lastoboma <lastoboma@gmail.com>
2025-05-02 03:55:09 +0000

    แก้ไข sites asia host

--- 0_drush.sh
+++ 0_drush.sh
@@ -5,14 +5,11 @@ read -p "Enter Drush Command: " command
 echo "drush $command -y mainsite"
 drush $command -y
 
-echo "drush $command -y stw24.com"
-drush $command -y --uri=stw24.com
+echo "drush $command -y 4pdecor.com"
+drush $command -y --uri=4pdecor.com
 
-echo "drush $command -y tamyah.com"
-drush $command -y --uri=tamyah.com
+echo "drush $command -y bpcarservice.com"
+drush $command -y --uri=bpcarservice.com
 
-echo "drush $command -y iosot.com"
-drush $command -y --uri=iosot.com
-
-echo "drush $command -y 8news.us"
-drush $command -y --uri=8news.us
+echo "drush $command -y xn--n3cfac0ercx4b8kvc.com"
+drush $command -y --uri=xn--n3cfac0ercx4b8kvc.com
