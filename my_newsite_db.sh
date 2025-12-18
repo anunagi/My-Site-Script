6d5369e1d7e493aec6874455a8a55f25191a00a5
anunagi <lastoboma@gmail.com>
2025-12-18 07:36:45 +0700

    Update my_newsite_db.sh
    ดัดแปลงแก้ไขใหม่โดย Antigravity+Gemini 3 Pro

--- my_newsite_db.sh
+++ my_newsite_db.sh
@@ -1,42 +1,40 @@
 #!/bin/sh
 
 # Put this script to /opt/bitnami/drupal/sites
+# Run it like: ./sites/my_newsite_db.sh <site> <dbname> <dbuser> <dbpass>.
 
-read -p "Enter New Site: " site
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <site_name> <db_name> <db_user> <db_password>"
+    exit 1
+fi
 
-echo "mkdir $site"
-mkdir $site
+site=$1
+dbname=$2
+dbuser=$3
+dbuserpass=$4
 
-echo "chmod 777 $site"
-chmod 777 $site
+echo "Creating site directory: $site"
+mkdir -p "$site"
+chmod 777 "$site"
 
-echo "mkdir $site/files"
-mkdir $site/files
+echo "Creating files directory"
+mkdir -p "$site/files"
+chmod 777 "$site/files"
 
-echo "chmod 777 $site/files"
-chmod 777 $site/files
+echo "Copying services.yml"
+cp default/default.services.yml "$site/services.yml"
+chmod 777 "$site/services.yml"
 
-echo "cp default/default.services.yml $site/services.yml"
-cp default/default.services.yml $site/services.yml
+echo "Copying settings.php"
+cp default/default.settings.php "$site/settings.php"
+chmod 777 "$site/settings.php"
 
-echo "chmod 777 $site/services.yml"
-chmod 777 $site/services.yml
-
-echo "cp default/default.settings.php $site/settings.php"
-cp default/default.settings.php $site/settings.php
-
-echo "chmod 777 $site/settings.php"
-chmod 777 $site/settings.php
-
-#get the database name
-read -p "Enter database name: " dbname
-read -p "Enter database user: " dbuser
-read -p "Enter database user password: " dbuserpass
+echo "Setting up database '$dbname' for user '$dbuser'"
 mysql -u root -p <<MYSQL_SCRIPT
-CREATE DATABASE $dbname;
-create user '$dbuser'@'127.0.0.1' identified by '$dbuserpass';
-grant all privileges on $dbname.* TO '$dbuser'@'127.0.0.1';
-flush privileges;
+CREATE DATABASE IF NOT EXISTS $dbname;
+CREATE USER IF NOT EXISTS '$dbuser'@'127.0.0.1' IDENTIFIED BY '$dbuserpass';
+GRANT ALL PRIVILEGES ON $dbname.* TO '$dbuser'@'127.0.0.1';
+FLUSH PRIVILEGES;
 MYSQL_SCRIPT
 
 echo "All done!"
